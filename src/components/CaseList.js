d6f49fa90f2cdcc417058875fd3d6a76ce4afeb1
albertbyrone <albertbyrone16777@gmail.com>
2021-01-09 07:46:11 +0300

    store variables in local states

--- src/components/CaseList.js
+++ src/components/CaseList.js
@@ -2,3 +2,6 @@ import React, { useState, useEffect } from "react";
 import CaseDataService from "../services/CaseService"
 import './CaseList.css';
 
+  const [cases, setCases] = useState([]);
+  const [currentCase, setCurrentCase] = useState(null);
+  const [message, setMessage] = useState("");
