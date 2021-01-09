2a81f4cb9a7bc227742cc54d5f8466139d6f38cb
albertbyrone <albertbyrone16777@gmail.com>
2021-01-09 07:40:40 +0300

    create an intitlal value of the case object

--- src/components/AddCase.js
+++ src/components/AddCase.js
@@ -1,3 +1,13 @@
 import  React, {useState}  from "react";
 import CaseDataService from "../services/CaseService"
 
+
+    const initialCaseState ={
+        id: 0,
+        date: "",
+        county: "",
+        cases: 0,
+        deaths: 0,
+        state: ""
+
+    };
