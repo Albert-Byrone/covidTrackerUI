262bba66011d7689b6b92d145d8c13b5863319ea
albertbyrone <albertbyrone16777@gmail.com>
2021-01-09 07:42:41 +0300

    create a funtion to create a new instance of case object

--- src/components/AddCase.js
+++ src/components/AddCase.js
@@ -19,3 +19,33 @@ import CaseDataService from "../services/CaseService"
         const { name, value} = event.target;
         setCase({..._case, [name]:value})
     };
+
+    // save data
+    const saveCase = ()=>{
+        var data = {
+            date: _case.date,
+            county: _case.county,
+            cases: _case.cases,
+            deaths: _case.deaths,
+            state: _case.state
+        }
+
+
+        CaseDataService.create(data)
+            .then(response => {
+                setCase({
+                    _id: response.data._id,
+                    date: response.data.date,
+                    county: response.data.county,
+                    cases: response.data.cases,
+                    deaths: response.data.deaths,
+                    state: response.data.state
+                });
+                setSubmitted(true)
+                console.log(data);
+
+            }).catch(e =>{
+                console.log(e);
+            });
+    };
+
