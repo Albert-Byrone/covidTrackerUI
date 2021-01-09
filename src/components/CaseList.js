0f562ffb06ce86a30b7584c7c983b5caea95c7b9
albertbyrone <albertbyrone16777@gmail.com>
2021-01-09 07:52:22 +0300

    addd a form to edit the data

--- src/components/CaseList.js
+++ src/components/CaseList.js
@@ -2,15 +2,16 @@ import React, { useState, useEffect } from "react";
 import CaseDataService from "../services/CaseService"
 import './CaseList.css';
 
+const CaseList = (props) => {
   const [cases, setCases] = useState([]);
   const [currentCase, setCurrentCase] = useState(null);
   const [message, setMessage] = useState("");
 
   useEffect(() => {
     retrieveCases();
-  }, []);
+    getCase(props.match.params._id);
+  }, [props.match.params._id]);
 
- 
   const retrieveCases = () => {
     CaseDataService.getAll()
       .then(response => {
@@ -34,6 +35,7 @@ import './CaseList.css';
         })
         .catch(e =>console.log(e))
 };
+
 
 
 const handleInputChange = event => {
@@ -93,3 +95,93 @@ const handleInputChange = event => {
         </tbody>  
         </table>
     </div>
+    <div className="col-md-2">
+    <div>
+        {currentCase ? (
+          <div className="edit-form">
+            <h4>Case</h4>
+            <form>
+              <div className="form-group">
+                <label htmlFor="title">Title</label>
+                <input
+                  type="Date"
+                  className="form-control"
+                  id="date"
+                  name="date"
+                  value={currentCase.date}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="county">County</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="county"
+                  name="county"
+                  value={currentCase.county}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="case">Case</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="case"
+                  name="case"
+                  value={currentCase.cases}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="death">Death</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="death"
+                  name="death"
+                  value={currentCase.deaths}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="state">State</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="state"
+                  name="state"
+                  value={currentCase.states}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </form>  
+            <button className="badge badge-danger mr-2" onClick={deleteCase}>
+              Delete
+            </button>
+  
+            <button
+              type="submit"
+              className="badge badge-success"
+              onClick={updateCase}
+            >
+              Update
+            </button>
+            <p>{message}</p>
+          </div>
+        ) : (
+          <div>
+            <br />
+            <p>Please click on a Case to edit...</p>
+          </div>
+        )}
+      </div>
+    </div>
+  </div>
+  );
+
+};
+export default CaseList;
